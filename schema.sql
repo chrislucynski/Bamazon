09da3eebeba89d118dd2cbf4bb1beeb097e25d2e
Chris Lucynski <chris.lucynski@gmail.com>
2019-10-14 21:32:50 -0600

    included products data

--- schema.sql
+++ schema.sql
@@ -16,6 +16,15 @@ select * from departments where department_name= 'Medical Supplies';
  
 
 --  <-----PRODUCTS----->
+INSERT INTO departments (product_name, department_name, price, stock_quantity, product_sales, department_id)
+	VALUES('heating pad', 'Medical Supplies', '375.00', '111', '1', '1'), 
+		('walker', 'Medical Supplies', '62.00', '25', '2', '1'),
+		('medical bed', 'Medical Supplies', '3750.00', '104', '75001', '1'),
+		('generic aspirin', 'Medications', '8.00', '600', '15', '2'),
+		('generic steroid cream', 'Medications', '12.00', '170', '1', '2'),
+		('generic beta-blocker', 'Medications', '10.00', '500', '8', '2');
+
+
 select * from products;
 
 UPDATE products set department_id = 2 where department_name = 'Medications';
@@ -36,16 +45,16 @@ d.over_head_costs
 order by p.department_id;
 
 -- OR THIS --
-SELECT d.department_id, p.department_name, 
-d.over_head_costs, p.product_sales,
-sum(p.product_sales - d.over_head_costs) as total_profit 
-FROM products p, departments d
-where p.department_id = d.department_id
-group by d.department_id, p.department_name, 
-d.over_head_costs, p.product_salesdepartments
-order by p.department_id;
+-- SELECT d.department_id, p.department_name, 
+-- d.over_head_costs, p.product_sales,
+-- sum(p.product_sales - d.over_head_costs) as total_profit 
+-- FROM products p, departments d
+-- where p.department_id = d.department_id
+-- group by d.department_id, p.department_name, 
+-- d.over_head_costs, p.product_salesdepartments
+-- order by p.department_id;
 
-SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', '')); 
+-- SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', '')); 
 
 
 
